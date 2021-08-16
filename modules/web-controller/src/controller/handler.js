d3893c58f0cbb0e4ef31fe9b321f705435f6f594
Prakash Sapkota <prsp20@gmail.com>
2021-08-16 20:25:01 +0100

    Add download report endpoint

--- modules/web-controller/src/controller/handler.js
+++ modules/web-controller/src/controller/handler.js
@@ -7,7 +7,6 @@ var AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-west-2'});
 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 const converter = AWS.DynamoDB.Converter;
-const ExcelJS = require('exceljs');
 
 export const handleUploadEvents = async (event, context, callback) => {
 
