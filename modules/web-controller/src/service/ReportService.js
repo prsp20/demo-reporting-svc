af13fa209460da3ff5fcc644644da0a9e80a0274
Prakash Sapkota <prsp20@gmail.com>
2021-08-16 20:58:48 +0100

    Fix import issue

--- modules/web-controller/src/service/ReportService.js
+++ modules/web-controller/src/service/ReportService.js
@@ -1,5 +1,10 @@
 import stringify from 'csv-stringify';
 
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-west-2'});
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const converter = AWS.DynamoDB.Converter;
+
 export const createReport = async (guid) => {
   const someData = [
     {
@@ -37,3 +42,25 @@ export const createReport = async (guid) => {
   const data = await stringify(someData, {header: true});
   return data;
 };
+
+export const findViewEvents = async (packageGuid) => {
+  const tableName = process.env.TABLE_NAME;
+  var params = {
+    ExpressionAttributeValues: {
+      ":packageGuid": {
+        S: packageGuid
+      }
+    },
+    FilterExpression: "packageGuid = :packageGuid",
+    TableName: tableName
+  };
+
+  const result = await ddb.scan(params).promise();
+
+  let items = result.Items;
+  if (items.length == 0) {
+    return null;
+  }
+
+  return items;
+}
