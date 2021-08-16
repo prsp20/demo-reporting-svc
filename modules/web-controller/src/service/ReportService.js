02b2c736aec793ce63bf4a85e74adff0a269548c
Prakash Sapkota <prsp20@gmail.com>
2021-08-16 20:22:08 +0100

    Add download report endpoint

--- modules/web-controller/src/service/ReportService.js
+++ modules/web-controller/src/service/ReportService.js
@@ -2,7 +2,7 @@ const ExcelJS = require('exceljs');
 
 export const createReport = (guid) => {
     const workbook = new ExcelJS.Workbook();
-    var worksheet = workbook.addWorksheet('VodViews');
+    let worksheet = workbook.addWorksheet('VodViews');
 
     worksheet.columns = [
       { header: 'Id', key: 'id', width: 10 },
@@ -11,9 +11,6 @@ export const createReport = (guid) => {
     ];
     worksheet.addRow({id: 1, name: 'John Doe', dob: new Date(1970,1,1)});
     worksheet.addRow({id: 2, name: 'Jane Doe', dob: new Date(1965,1,7)});
-
-    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
 
     return workbook;
 };
